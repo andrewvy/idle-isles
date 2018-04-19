6cbd5422b90699b05d7016f906cc16e68f52236d
Andrew Vy <andrew@andrewvy.com>
2018-04-19 12:25:37 -0700

    :sparkles: Add react router

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -3,18 +3,23 @@ import 'whatwg-fetch'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { render } from 'react-dom'
+import { ConnectedRouter } from 'react-router-redux'
+import { Route } from 'react-router'
 
 import 'bulma'
 
 import App from '~/components/app'
 import Store from '~/store'
+import History from '~/lib/history'
 
 const wrapper = document.getElementById('app')
 
 if (wrapper) {
   render(
     <Provider store={Store}>
-      <App />
+      <ConnectedRouter history={History}>
+        <Route exact path='/' component={App} />
+      </ConnectedRouter>
     </Provider>
   , wrapper
   )
--- assets/js/store/index.js
+++ assets/js/store/index.js
@@ -1,12 +1,23 @@
 import * as Redux from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { apiMiddleware } from 'redux-api-middleware';
+
+import thunkMiddleware from 'redux-thunk'
+import History from '~/lib/history'
+import { Route } from 'react-router'
+import { apiMiddleware } from 'redux-api-middleware'
+import { routerReducer, routerMiddleware } from 'react-router-redux'
 
 import Reducers from '~/reducers';
 
 const Store = ((_Redux, _Reducers) => {
-  const middleware = _Redux.applyMiddleware(thunkMiddleware, apiMiddleware);
-  const reducers = _Redux.combineReducers(_Reducers);
+  const middleware = _Redux.applyMiddleware(
+    thunkMiddleware,
+    apiMiddleware,
+    routerMiddleware(History)
+  );
+
+  const reducers = _Redux.combineReducers(_Reducers, {
+    router: routerReducer,
+  });
 
   return Redux.createStore(reducers, {}, Redux.compose(
     middleware,
